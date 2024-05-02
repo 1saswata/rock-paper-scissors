3aaaaac8f6b4f425a9e53c6f30c93b4475ffd558
Saswata Gorai <13601653+1saswata@users.noreply.github.com>
2024-05-02 15:50:56 +0530

    feat: Create getHumanChoice function
    This function takes the user choice as input and
    returns it

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let randomRock = Math.random();
     let randomPaper = Math.random();
@@ -11,3 +14,24 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let userChoice = "";
+    let userChoiceNumber = Number(prompt("Enter your pick (1/2/3) - 1 for Rock/ 2 for Paper/ 3 for Scissors:"));
+    switch (userChoiceNumber) {
+        case 1:
+            userChoice = "rock";
+            break;
+        case 2:
+            userChoice = "paper";
+            break;
+        case 3:
+            userChoice = "scissors";
+            break;
+        default:
+            userChoice = null;
+    }
+    return userChoice;
+}
+
+console.log(getHumanChoice());
