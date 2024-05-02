9242dde3941f8a0f73b35682617cd5f78c24b5b7
Saswata Gorai <13601653+1saswata@users.noreply.github.com>
2024-05-02 14:21:37 +0530

    feat: Create getComputerChoice function
    getComputerChoice randomly returns one of the
    following string values: “rock”, “paper” or
    “scissors”.

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-console.log("Test");
+function getComputerChoice() {
+    let randomRock = Math.random();
+    let randomPaper = Math.random();
+    let randomScissors = Math.random();
+    if (randomRock >= randomPaper && randomRock >= randomScissors)
+        return "rock";
+    else if (randomPaper >= randomRock && randomPaper >= randomScissors)
+        return "paper";
+    else
+        return "scissors";
+}
+
+console.log(getComputerChoice());
