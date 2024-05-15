0ede6a2f0699877bd8d24c19b00236fe7344ad6a
Saswata Gorai <13601653+1saswata@users.noreply.github.com>
2024-05-15 18:39:10 +0530

    feat: Add working UI for the game
    Working UI is added including functioning event-
    listeners to play the game.

--- game.js
+++ game.js
@@ -19,73 +19,76 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    function playRound(humanChoice, computerChoice) {
-        console.log(`Computer has chosen ${computerChoice}.`);
-        //If both choices are same, no one wins
-        if(humanChoice === computerChoice) 
-            console.log("Draw! No score.");
-        else {
-        //If choices aren't same, rock beats scissors, paper beats rock, scissors 
-        //beat paper. 
-            switch (humanChoice) {
-                case "rock":
-                    if (computerChoice === "scissors") {
-                        humanScore++;
-                        console.log(`You win! ${humanChoice + " beats " 
-                        + computerChoice}.`);
-                    }
-                    else {
-                        computerScore++;
-                        console.log(`You lose! ${computerChoice + " beats " 
-                        + humanChoice}.`);
-                    }
-                    break;
-                case "paper":
-                    if (computerChoice === "rock") {
-                        humanScore++;
-                        console.log(`You win! ${humanChoice + " beats " 
-                        + computerChoice}.`);
-                    }
-                    else {
-                        computerScore++;
-                        console.log(`You lose! ${computerChoice + " beats " 
-                        + humanChoice}.`);
-                    }
-                    break;
-                case "scissors":
-                    if (computerChoice === "paper") {
-                        humanScore++;
-                        console.log(`You win! ${humanChoice + " beats " 
-                        + computerChoice}.`);
-                    }
-                    else {
-                        computerScore++;
-                        console.log(`You lose! ${computerChoice + " beats " 
-                        + humanChoice}.`);
-                    }
-                    break;
-            }
+let humanScore = 0;
+let computerScore = 0;
+let newGame = 1;
+function playRound(humanChoice, computerChoice) {
+    if (newGame === 1) {
+        result.textContent = "\nStarting a new game! \n";
+        newGame = 0;
+    }
+    result.textContent += `----------------------\nRound ${humanScore + computerScore + 1}:\n`;
+    result.textContent += `Computer has chosen ${computerChoice}.\n`;
+    result.textContent += `You have chosen ${humanChoice}.\n`;
+    //If both choices are same, no one wins
+    if(humanChoice === computerChoice) 
+        result.textContent += "Draw! No score. Redo the round!\n";
+    else {
+    //If choices aren't same, rock beats scissors, paper beats rock, scissors 
+    //beat paper. 
+        switch (humanChoice) {
+            case "rock":
+                if (computerChoice === "scissors") {
+                    humanScore++;
+                    result.textContent += `You win! ${humanChoice + " beats " 
+                    + computerChoice}.\n`;
+                }
+                else {
+                    computerScore++;
+                    result.textContent += `You lose! ${computerChoice + " beats " 
+                    + humanChoice}.\n`;
+                }
+                break;
+            case "paper":
+                if (computerChoice === "rock") {
+                    humanScore++;
+                    result.textContent += `You win! ${humanChoice + " beats " 
+                    + computerChoice}.\n`;
+                }
+                else {
+                    computerScore++;
+                    result.textContent += `You lose! ${computerChoice + " beats " 
+                    + humanChoice}.\n`;
+                }
+                break;
+            case "scissors":
+                if (computerChoice === "paper") {
+                    humanScore++;
+                    result.textContent += `You win! ${humanChoice + " beats " 
+                    + computerChoice}.\n`;
+                }
+                else {
+                    computerScore++;
+                    result.textContent += `You lose! ${computerChoice + " beats " 
+                    + humanChoice}.\n`;
+                }
+                break;
+        }
+        result.textContent += `Current Score -\nYou: ${humanScore} | Computer: ${computerScore}\n`;
+        if (humanScore + computerScore === 5) {
+            if(humanScore > computerScore)
+                result.textContent += "\nCongrats, you win the game!";
+            else
+            result.textContent += "\nNT, you lose the game.";
+            humanScore = 0;
+            computerScore = 0;
+            newGame = 1;
         }
     }
-    let humanSelection = "";
-    let computerSelection= "";
-    console.log(`Round ${i+1}: `)
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    console.log(`Current Score -\n 
-    You: ${humanScore} | Computer: ${computerScore}\n`
-    if(humanScore > computerScore)
-        console.log("Congrats, you win!");
-    else if (humanScore === computerScore)
-        console.log("GG, it's a draw.");
-    else
-        console.log("NT, you lose.");
 }
-
-playGame();
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button)=> {
+    button.addEventListener("click", ()=>playRound(button.textContent.toLowerCase(), getComputerChoice()));
+    }
+)
+const result = document.querySelector("#result");
